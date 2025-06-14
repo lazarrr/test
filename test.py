3df7cb710aa9206fbc4756e69f426febc7446317
LAZAR KNEZEVIC <lazarknezevic98@gmail.com>
2025-06-14 22:54:05 +0200

    tt
    some other commmt
    
    one more info commit

---
+++ test.py
@@ -0,0 +1,38 @@
+import random
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return f"{self.name} makes a sound."
+
+class Car:
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+
+    def honk(self):
+        return f"{self.brand} car honks!"
+
+    def get_info(self):
+        print(f"Getting info for {self.brand} car from {self.year}")
+        print("This is a car object.")
+        return f"{self.year} {self.brand}"
+
+
+class Dice:
+    def __init__(self, sides=6):
+        self.sides = sides
+
+    def roll(self):
+        return random.randint(1, self.sides)
+    
+    def __str__(self):
+        return f"Dice with {self.sides} sides"
+
+    def __repr__(self):
+        return f"Dice({self.sides})"
+
+    def is_fair(self):
+        return self.sides >= 2
