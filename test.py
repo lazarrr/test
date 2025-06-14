f03dcc2077ce5462e17e978691d01bc68f76b684
LAZAR KNEZEVIC <lazarknezevic98@gmail.com>
2025-06-14 23:22:48 +0200

    fsdf

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ class Car:
     def get_info(self):
         print(f"Getting info for {self.brand} car from {self.year}")
         print("This is a car object.")
+        return f"{self.brand} {self.year}"
         return f"{self.year} {self.brand}"
 
 
