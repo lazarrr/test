cff669d5179751ea31db99dffe558e8b1a6c84d0
LAZAR KNEZEVIC <lazarknezevic98@gmail.com>
2025-06-15 20:27:06 +0200

    new commit
    first
    
    new class more code

--- test2.py
+++ test2.py
@@ -13,3 +13,25 @@ class Car:
         self.brand = brand
         self.year = year
 
+    def honk(self):
+        return f"{self.brand} car honks!"
+    
+    def get_info(self):
+        print(f"Getting info for {self.brand} car from {self.year}")
+        print("This is a car object.")
+        print("It has a brand and a year.")
+        return f"{self.brand} {self.year}"
+    
+  
+    
+class Dice:
+
+    def __init__(self):
+        self.sides = 6
+
+    def __str__(self):
+        return f"Dice with {self.sides} sides"
+
+    def roll(self):
+        print(f"Rolling a {self.sides}-sided dice...")
+        return random.randint(1, self.sides)
